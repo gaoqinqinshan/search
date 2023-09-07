80ca3890419d84ff33cdd8046339d76dac5005f2
gaoqinqinshan <1626733251@qq.com>
2023-09-07 23:04:17 +0800

    配置 Config

---
+++ src/main/java/com/search/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.search.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域问题
+ *
+ * WebMvcConfigurer 需要注意，这个就是拦截器的重写源头
+ * 一般来说，就是重写它来解决跨域的问题的
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
